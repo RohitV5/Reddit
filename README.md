7a19897a5b664afc39880263ccb01366396b3b0a
Rohit Verma <verma.rohit.in@gmail.com>
2022-06-23 17:20:27 +0530

    feat: reddit form and graphql apollo config

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Reddit Clone
+
 NextJS
 React
 SQL
@@ -7,3 +8,14 @@ StepZen
 GraphQL
 Typescript
 Tailwind
+React hook form
+
+Create postgres db in supabase,
+connect it with stepzen graphql service using stepzen init
+start stepzen server using stepzen start
+stepzen with automatically generate all the schema for your database
+which can be accessed at localhost:5000
+
+for NEXT_PUBLIC_STEPZEN_KEY ==>stepzen whoami --apikey
+
+Dicebear avatars for random avatars.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
 import { SessionProvider } from 'next-auth/react';
 import Header from '../components/Header';
+import client from '../apollo-client';
 
 const MyApp = function ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className="h-screen overflow-y-scroll bg-slate-200">
-        <Header />
-        <Component {...pageProps} />;
-      </div>
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <div className="h-screen overflow-y-scroll bg-slate-200">
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </ApolloProvider>
   );
 };
 
